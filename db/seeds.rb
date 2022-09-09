63663e2d436517bf02b607506b9bef4ffd316bf1
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-09 20:16:23 +0300

    seed student data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+10.times do 
+    Student.create(
+        first_name: Faker::Name.name,
+        last_name: Faker::Name.unique.name,
+        gender: Faker::Gender.binary_type,
+        email: Faker::Internet.email,
+        password: Faker::Alphanumeric.alphanumeric(number: 10))
+    end 
+
+    rand(1..10).times do
+        student = Student.order('RANDOM()').first 
+
+        
 puts "✅ Done seeding!"
