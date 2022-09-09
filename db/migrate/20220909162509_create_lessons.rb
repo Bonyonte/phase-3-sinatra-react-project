a3c652d72cd647728fe34cbb3d5ea23725e0b4be
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-09 19:33:31 +0300

    create lessons table

--- db/migrate/20220909162509_create_lessons.rb
+++ db/migrate/20220909162509_create_lessons.rb
@@ -1,4 +1,10 @@
 class CreateLessons < ActiveRecord::Migration[6.1]
   def change
+    create_table :lessons do |t|
+      t.string :lesson_name
+      t.string :lesson_teacher
+      t.string :progress 
+      t.timestamps
+    end
   end
 end
